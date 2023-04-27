1ff64b0002289ad93f8070a74cfadc07001ddfa2
debora-rebelatto <deborarebelatto1@gmail.com>
2023-04-27 11:25:45 -0300

    identation

--- trabalhos/trab3-vetor.asm
+++ trabalhos/trab3-vetor.asm
@@ -5,9 +5,9 @@
 	resultado: .asciiz "Maior valor do array:  "
 .text
 main:
- 	la t0, vetor               # load address of vetor into x1
+  la t0, vetor               # load address of vetor into x1
 	lw t1, tamanho             # load tamanho into t0
- 	li s0, 0                   # initialize maior to 0
+  li s0, 0                   # initialize maior to 0
 	li s1, 0                   # initialize x to 0
 loop_start:
   lw s2, 0(t0)               # load vetor[x] into s2
